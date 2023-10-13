649297a2daa9b2ab645b6e4d591e7ac1df88084b
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-10-13 19:43:10 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(UserName, userEmail) {
+    const formData = {
+      name: UserName,
+      email: userEmail,
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        const element = document.createElement('h1');
+        element.textContent = object.id;
+        document.body.append(element);
+      })
+      .catch(function (error) {
+        const errorMessage = document.createElement('p')
+        errorMessage.textContent = error
+        document.body.append(errorMessage)
+      });
+  }
